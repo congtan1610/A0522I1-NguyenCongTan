c35e98682a9ac58dc3bfd4da04e98ee528bf8907
congtan1610 <congtan125@gmail.com>
2022-06-15 18:52:18 +0700

    js lan 1

---
+++ ss6-javascript/thực hành/demo.js
@@ -0,0 +1,34 @@
+function mouseover(nameId) {
+    document.getElementById(nameId).style.color = "red";
+}
+
+function mouseout(nameId) {
+    document.getElementById(nameId).style.color = "black";
+}
+
+// var gg=document.getElementById("gg");
+// gg.addEventListener("mouseover",mouseover1);
+// gg.addEventListener("mouseout",mouseout1);
+// function mouseover1() {
+//     document.getElementById("gg").style.color="red";          cach 2
+// }
+// function mouseout1() {
+//     document.getElementById("gg").style.color="black";
+// }
+function hienthi(nameId) {
+    alert('nguyen cong tan');
+}
+
+function hienthi1(nameId) {
+        document.getElementById(nameId).innerText = 'nguyen cong tan';
+}
+function hienthi2(nameId) {
+    document.getElementById(nameId).innerText = 'nct';
+}
+function hienthi3(nameId) {
+
+}
+
+
+
+
