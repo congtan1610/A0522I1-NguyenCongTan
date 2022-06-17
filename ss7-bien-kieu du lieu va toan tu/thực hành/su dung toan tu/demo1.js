6b4f4f6012da64717c65b0d08daf5a70b0d88078
congtan1610 <congtan125@gmail.com>
2022-06-18 01:31:33 +0700

    ss7 lan 1

---
+++ ss7-bien-kieu du lieu va toan tu/thực hành/su dung toan tu/demo1.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("rong");
+inputHeight = prompt("cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("dien tich hinh chu nhat la: " + area);
